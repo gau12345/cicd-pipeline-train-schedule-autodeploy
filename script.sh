f0fc0abcc0fcc138fa6425983c65e58ef51c05a0
Gaurav Tadas <59065700+gau12345@users.noreply.github.com>
2024-06-06 00:07:24 +0530

    Update script.sh

--- script.sh
+++ script.sh
@@ -1,4 +1,6 @@
 sudo -s
+sudo cd /home/ubuntu/workDir/
+sudo git clone https://github.com/gau12345/cicd-pipeline-train-schedule-autodeploy.git
 sudo cd /home/ubuntu/workDir/cicd-pipeline-train-schedule-autodeploy
 sudo docker build -t cicd-pipeline-train-schedule-autodeploy:latest .
 sudo docker push cicd-pipeline-train-schedule-autodeploy:latest
